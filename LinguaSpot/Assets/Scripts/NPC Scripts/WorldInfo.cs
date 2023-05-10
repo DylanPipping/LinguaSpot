4a9a26c36b3865dd8ab9974238188de16744faa1
Pipping,Dylan D.F.J <352995@student.fontys.nl>
2023-05-10 10:14:18 +0200

    Changed and added files for chatGPT and models

---
+++ LinguaSpot/Assets/Scripts/NPC Scripts/WorldInfo.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class WorldInfo : MonoBehaviour
+{
+    [SerializeField, TextArea] private string gameStory;
+    [SerializeField, TextArea] private string gameWorld;
+}
